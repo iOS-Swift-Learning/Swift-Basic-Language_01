0c7784db2c508f7b752978cc8a2cf6350e3768ae
XCODE <XCODE@FM-D2deiMac.local>
2015-09-22 19:16:54 +0800

    Initial Commit

---
+++ HelloWorld/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by XCODE on 2015/9/22.
+//  Copyright (c) 2015年 NickLas. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
